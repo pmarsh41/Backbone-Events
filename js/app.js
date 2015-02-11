db9aad5ff9caf4e7dd3a5206cc3ef6e601f82364
Phil <marshall.phillip@gmail.com>
2015-02-11 00:07:14 -0600

    .

--- js/app.js
+++ js/app.js
@@ -10,11 +10,14 @@ function app(){
         {url: "./dist/style.css"},
         //js
         {url: "./bower_components/jquery/dist/jquery.min.js"},
-        {url: "./bower_components/lodash/lodash.min.js"}
-        // {url: "./bower_components/backbone/backbone.js"}
+        {url: "./bower_components/lodash/lodash.min.js"},
+        {url: "./bower_components/backbone/backbone.js"},
+        {url: "./js/viewClient.js"}
     ).then(function(){
         document.querySelector("html").style.opacity = 1;
         // start app?
+
+        var goClient = new ViewClient()
     })
 
 }
